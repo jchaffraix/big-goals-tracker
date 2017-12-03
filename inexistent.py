7edf66b3ca8e380d519d4b72b1ebada67155d6b7
Julien Chaffraix <julien.chaffraix@gmail.com>
2017-12-02 20:10:57 -0800

    Fix the inexistent path
    The path was relative so we
    would miss the image on sub-paths.

--- inexistent.py
+++ inexistent.py
@@ -10,7 +10,7 @@ class ErrorPage(webapp2.RequestHandler):
 <!DOCTYPE html>
 The page you're requesting doesn't exist!<br>
 <a href="https://www.flickr.com/photos/armydre2008/19339086155/">
-    <img src="images/shocked-raccoon.jpg">
+    <img src="/images/shocked-raccoon.jpg">
 </a>
 <!-- Credit: User frankieleon on flickr -->''')
 
